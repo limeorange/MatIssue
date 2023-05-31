b968d2af176ed9a70b9592fe9dc045da5227ed12
limeorange <happyshsh72@gmail.com>
2023-05-31 16:35:07 +0900

    Feat : 레시피 조회 페이지 스크롤 진행바 구현

--- front/app/(page)/recipe/[id]/ViewPageClient.tsx
+++ front/app/(page)/recipe/[id]/ViewPageClient.tsx
@@ -3,105 +3,123 @@
 import IngredientList from "@/app/components/recipe/IngredientList";
 import RecipeInfo from "@/app/components/recipe/RecipeInfo";
 import RecipeSteps from "@/app/components/recipe/RecipeSteps";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import styled from "styled-components";
 
-// 요리 대표 이미지
+// 대표 이미지, 제목, 작성자, 소개글,
 const recipeCover = "/요리 대표 사진.png";
-
-// 요리 제목
 const recipeTitle = "보들보들 순두부 달걀탕";
-
-// 작성자
 const author = "냠냠순두부러버";
-
-// 간단 소개글
 const description = `보들보들 부드러운 순두부 달걀탕.
 순두부와 달걀만 있으면 간단하게 만들 수 있는 순두부 달걀탕 레시피를 소개해 드릴게요. 
 달걀탕 더욱 더 맛있게 먹는 법! 더욱 더 시원하고 깔끔한 맛을 내려면 새우젓을 넣어보세요.
 부드럽고 소화가 잘되기 때문에 아침 식사로도 제격이랍니다. 
 쌀쌀한 아침은 따끈한 순두부 달걀탕으로 어떠세요?`;
 
-// 요리팁
+// 요리팁, 동영상 이미지, 동영상 링크, 댓글 개수
 const recipeTip = "멸치액젓이 싫으시면 국간장 2T만으로 간 하셔도 좋습니다.";
-
-// 요리 동영상 이미지
 const recipeVideoImage = "/요리 동영상.png";
-
-// 요리 동영상 링크
 const recipeVideoUrl = "https://youtu.be/jk29M4knFBw";
-
-// 댓글 개수
 const commentCount = 3;
 
 // 레시피 조회 페이지 컴포넌트
 const RecipeDetail = () => {
+  // 동영상 썸네일 클릭 핸들러
   const imageClickHandler = () => {
     window.open(recipeVideoUrl);
   };
+
+  // 스크롤 진행 퍼센트 상태 관리
+  const [scrollPercentage, setScrollPercentage] = useState(0);
+
+  useEffect(() => {
+    // 스크롤 진행 퍼센트를 현재 스크롤 위치를 기반으로 계산해주는 핸들러
+    const scrollHandler = () => {
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+      const windowHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+      const percentage = (scrollTop / windowHeight) * 100;
+      setScrollPercentage(percentage);
+    };
+
+    // 스크롤이 발생할 때마다 스크롤 진행 퍼센트 계산
+    window.addEventListener("scroll", scrollHandler);
+
+    // 컴포넌트가 언마운트될 때 이전에 등록한 스크롤 이벤트 리스너 제거 => 메모리 누수 방지
+    return () => window.removeEventListener("scroll", scrollHandler);
+  }, []);
+
   return (
-    <ContainerDiv>
-      {/* 요리 대표 이미지 */}
-      <ImageWrapperDiv>
-        <Image
-          src={recipeCover}
-          alt="요리 대표 사진"
-          fill
-          style={{ objectFit: "cover", borderRadius: 20 }}
-        />
-      </ImageWrapperDiv>
+    <>
+      <ContainerDiv>
+        {/* 스크롤 상태 진행바 */}
+        <ProgressBarContainer>
+          <ProgressBar progress={scrollPercentage} />
+        </ProgressBarContainer>
 
-      {/* 요리 제목, 작성자, 간단 소개글 */}
-      <div>
-        <TitleContainerDiv>
-          <TitleH3>{recipeTitle}</TitleH3>
-          <AuthorSpan>by {author}</AuthorSpan>
-        </TitleContainerDiv>
-        <DescriptionDiv>{description}</DescriptionDiv>
-      </div>
+        {/* 요리 대표 이미지 */}
+        <ImageWrapperDiv>
+          <Image
+            src={recipeCover}
+            alt="요리 대표 사진"
+            fill
+            style={{ objectFit: "cover", borderRadius: 20 }}
+          />
+        </ImageWrapperDiv>
 
-      {/* 요리 정보 (인원, 시간, 난이도, 종류) */}
-      <div>
-        <SubtitleH2>요리 정보</SubtitleH2>
-        <RecipeInfo></RecipeInfo>
-      </div>
+        {/* 요리 제목, 작성자, 간단 소개글 */}
+        <div>
+          <TitleContainerDiv>
+            <TitleH3>{recipeTitle}</TitleH3>
+            <AuthorSpan>by {author}</AuthorSpan>
+          </TitleContainerDiv>
+          <DescriptionDiv>{description}</DescriptionDiv>
+        </div>
 
-      {/* 재료 준비 목록 */}
-      <div>
-        <SubtitleH2>재료 준비</SubtitleH2>
-        <IngredientList></IngredientList>
-      </div>
+        {/* 요리 정보 (인원, 시간, 난이도, 종류) */}
+        <div>
+          <SubtitleH2>요리 정보</SubtitleH2>
+          <RecipeInfo></RecipeInfo>
+        </div>
 
-      {/* 요리 과정 */}
-      <div>
-        <SubtitleH2>요리 과정</SubtitleH2>
-        <RecipeSteps></RecipeSteps>
-      </div>
+        {/* 재료 준비 목록 */}
+        <div>
+          <SubtitleH2>재료 준비</SubtitleH2>
+          <IngredientList></IngredientList>
+        </div>
 
-      {/* 요리팁 */}
-      <div>
-        <SubtitleH2>요리팁</SubtitleH2>
-        <RecipeTipDiv>{recipeTip}</RecipeTipDiv>
-      </div>
+        {/* 요리 과정 */}
+        <div>
+          <SubtitleH2>요리 과정</SubtitleH2>
+          <RecipeSteps></RecipeSteps>
+        </div>
 
-      {/* 요리 동영상 */}
-      <div>
-        <SubtitleH2>요리 동영상</SubtitleH2>
-        <Image
-          src={recipeVideoImage}
-          alt="요리 동영상"
-          width={380}
-          height={210}
-          style={{ cursor: "pointer" }}
-          onClick={imageClickHandler}
-        ></Image>
-        <VideoDescriptionDiv>
-          썸네일 클릭 시 동영상 링크로 연결됩니다.
-        </VideoDescriptionDiv>
-      </div>
+        {/* 요리팁 */}
+        <div>
+          <SubtitleH2>요리팁</SubtitleH2>
+          <RecipeTipDiv>{recipeTip}</RecipeTipDiv>
+        </div>
 
-      {/* 댓글 */}
-      <div>
+        {/* 요리 동영상 */}
+        <div>
+          <SubtitleH2>요리 동영상</SubtitleH2>
+          <Image
+            src={recipeVideoImage}
+            alt="요리 동영상"
+            width={380}
+            height={210}
+            style={{ cursor: "pointer" }}
+            onClick={imageClickHandler}
+          ></Image>
+          <VideoDescriptionDiv>
+            썸네일 클릭 시 동영상 링크로 연결됩니다.
+          </VideoDescriptionDiv>
+        </div>
+
+        {/* 댓글 */}
         <div className="flex">
           <SubtitleH2>댓글</SubtitleH2>
           <CommentIconDiv>
@@ -114,8 +132,8 @@ const RecipeDetail = () => {
           </CommentIconDiv>
           <SubtitleH2>{commentCount}</SubtitleH2>
         </div>
-      </div>
-    </ContainerDiv>
+      </ContainerDiv>
+    </>
   );
 };
 
@@ -179,6 +197,23 @@ const VideoDescriptionDiv = styled.div`
   padding-top: 0.5rem;
   color: #6f6f6f;
   font-size: 1.5rem;
+`;
+
+// 스크롤 진행바 전체 박스
+const ProgressBarContainer = styled.div`
+  position: fixed;
+  left: 0;
+  width: 100%;
+  height: 10px;
+  background-color: #f2f2f2;
+  z-index: 40;
+`;
+
+// 스크롤 진행바
+const ProgressBar = styled.div<{ progress: number }>`
+  height: 100%;
+  width: ${({ progress }) => `${progress}%`};
+  background-color: #fbd26a;
 `;
 
 export default RecipeDetail;
