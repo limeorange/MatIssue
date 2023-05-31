fc94cfc4459ce9f36476530fd5e9a08706b2a188
limeorange <happyshsh72@gmail.com>
2023-05-31 16:49:19 +0900

    Refactor : 스크롤 진행바 컴포넌트 분리

--- front/app/(page)/recipe/[id]/ViewPageClient.tsx
+++ front/app/(page)/recipe/[id]/ViewPageClient.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import IngredientList from "@/app/components/recipe/IngredientList";
+import ProgressBar from "@/app/components/recipe/ProgressBar";
 import RecipeInfo from "@/app/components/recipe/RecipeInfo";
 import RecipeSteps from "@/app/components/recipe/RecipeSteps";
-import { useState, useEffect } from "react";
 import Image from "next/image";
 import styled from "styled-components";
 
@@ -30,35 +30,11 @@ const RecipeDetail = () => {
     window.open(recipeVideoUrl);
   };
 
-  // 스크롤 진행 퍼센트 상태 관리
-  const [scrollPercentage, setScrollPercentage] = useState(0);
-
-  useEffect(() => {
-    // 스크롤 진행 퍼센트를 현재 스크롤 위치를 기반으로 계산해주는 핸들러
-    const scrollHandler = () => {
-      const scrollTop =
-        window.pageYOffset || document.documentElement.scrollTop;
-      const windowHeight =
-        document.documentElement.scrollHeight -
-        document.documentElement.clientHeight;
-      const percentage = (scrollTop / windowHeight) * 100;
-      setScrollPercentage(percentage);
-    };
-
-    // 스크롤이 발생할 때마다 스크롤 진행 퍼센트 계산
-    window.addEventListener("scroll", scrollHandler);
-
-    // 컴포넌트가 언마운트될 때 이전에 등록한 스크롤 이벤트 리스너 제거 => 메모리 누수 방지
-    return () => window.removeEventListener("scroll", scrollHandler);
-  }, []);
-
   return (
     <>
       <ContainerDiv>
         {/* 스크롤 상태 진행바 */}
-        <ProgressBarContainer>
-          <ProgressBar progress={scrollPercentage} />
-        </ProgressBarContainer>
+        <ProgressBar />
 
         {/* 요리 대표 이미지 */}
         <ImageWrapperDiv>
@@ -197,24 +173,6 @@ const VideoDescriptionDiv = styled.div`
   padding-top: 0.5rem;
   color: #6f6f6f;
   font-size: 1.5rem;
-`;
-
-// 스크롤 진행바 전체 박스
-const ProgressBarContainer = styled.div`
-  position: fixed;
-  left: 0;
-  width: 100%;
-  height: 10px;
-  background-color: #f2f2f2;
-  z-index: 40;
-`;
-
-// 스크롤 진행바
-const ProgressBar = styled.div<{ progress: number }>`
-  height: 100%;
-  width: ${({ progress }) => `${progress}%`};
-  background-color: #fbd26a;
-  border-radius: 0 10px 10px 0;
 `;
 
 export default RecipeDetail;
