22d44c090d66cce446eada3acb5975bd2e060071
jang_yoonsu <shagrat8546@gmail.com>
2023-06-02 00:46:24 +0900

    Design : 토스터 스타일 수정

--- front/app/context/ToasterContext.tsx
+++ front/app/context/ToasterContext.tsx
@@ -1,9 +1,38 @@
 "use client";
 
-import { Toaster } from "react-hot-toast";
+import { ToastBar, Toaster } from "react-hot-toast";
 
 const ToasterContext = () => {
-  return <Toaster />;
+  return (
+    <Toaster
+      toastOptions={{
+        style: {
+          fontSize: "16px",
+          padding: "12px 16px",
+          color: "#4F3D21",
+          backgroundColor: "#FBE2A1",
+        },
+        success: {
+          iconTheme: {
+            primary: "#5CB27A",
+            secondary: "white",
+          },
+        },
+      }}
+    >
+      {(t) => (
+        <ToastBar
+          toast={t}
+          style={{
+            ...t.style,
+            animation: t.visible
+              ? "custom-enter 1s ease"
+              : "custom-exit 1s ease",
+          }}
+        />
+      )}
+    </Toaster>
+  );
 };
 
 export default ToasterContext;
