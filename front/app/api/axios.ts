532c5ae915d538ac9a1e9976f563d6f85b75cbcb
jang_yoonsu <shagrat8546@gmail.com>
2023-06-02 15:01:28 +0900

    Feat : axios 설정에서 url에 api 추가

--- front/app/api/axios.ts
+++ front/app/api/axios.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-// axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true;
 
 // Axios 인스턴스 생성
 export const axiosBase = axios.create({
-  baseURL: "https://matissue.onrender.com/",
+  baseURL: "https://matissue.onrender.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
