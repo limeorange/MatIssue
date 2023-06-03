f4756cf8233ea93d6c82bebe5c43fe1c5da9359d
limeorange <happyshsh72@gmail.com>
2023-06-03 21:27:25 +0900

    Rename : 폴더명 recipe -> recipes로 변경

--- front/app/(page)/recipe/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import RecipeDetail from "./ViewPageClient";
-
-// 레시피 조회 페이지
-const ViewPage = ({ params }: { params: { id: string } }) => {
-  console.log(params.id);
-  return <RecipeDetail />;
-};
-
-export default ViewPage;
