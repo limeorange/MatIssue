ee0f8e50105bba15056bdab33b5f6c5a887b1cbd
yubin <shin.yubin18@gmail.com>
2023-06-03 11:06:05 +0900

    Add : aws imageUploader 완성
    사진 업로드 시 참고 바랍니다.
    npm i 필요

--- front/app/api/aws.ts
+++ front/app/api/aws.ts
@@ -1,11 +1,10 @@
-//@ts-nocheck
 import dotenv from "dotenv";
-import AWS from "aws-sdk";
+import AWS, { S3 } from "aws-sdk";
 
 dotenv.config();
 
-const ID = process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID;
-const SECRET = process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY;
+const ID: string = process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID!;
+const SECRET: string = process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY!;
 
 /** aws configuration */
 AWS.config.update({
@@ -14,31 +13,33 @@ AWS.config.update({
   region: "ap-northeast-2",
 });
 
-const s3 = new AWS.S3({
+const s3: S3 = new AWS.S3({
   accessKeyId: ID,
   secretAccessKey: SECRET,
 });
 
 /** 날짜 변환 코드 : 이미지 이름에 사용됩니다. */
-const currentDateTime = new Date();
-const formattedDateTime = currentDateTime
+const currentDateTime: Date = new Date();
+const formattedDateTime: string = currentDateTime
   .toISOString()
   .replace(/[-:.TZ]/g, "")
   .replace(/T/g, "-")
   .substring(0, 17);
 
-export default async function uploadImage(file: File) {
+export default async function uploadImage(
+  file: File
+): Promise<{ imageUrl: string }> {
   try {
-    const fileKey = `${formattedDateTime}_${file.name}`;
+    const fileKey: string = `${formattedDateTime}_${file.name}`;
     console.log(process.env.NEXT_PUBLIC_AWS_BUCKET_NAME);
-    const params = {
-      Bucket: process.env.NEXT_PUBLIC_AWS_BUCKET_NAME,
+    const params: S3.PutObjectRequest = {
+      Bucket: process.env.NEXT_PUBLIC_AWS_BUCKET_NAME!,
       Key: fileKey,
       Body: file,
       ACL: "public-read",
     };
     await s3.upload(params).promise();
-    const imageUrl = `https://${process.env.NEXT_PUBLIC_AWS_BUCKET_NAME}.s3.amazonaws.com/${fileKey}`;
+    const imageUrl: string = `https://${process.env.NEXT_PUBLIC_AWS_BUCKET_NAME}.s3.amazonaws.com/${fileKey}`;
     return { imageUrl };
   } catch (error) {
     console.error("Image upload failed:", error);
