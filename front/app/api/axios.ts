135c7e80b4f761d63769b72c05e934160156d4cb
jang_yoonsu <shagrat8546@gmail.com>
2023-06-04 18:45:41 +0900

    Feat : axios 설정

--- front/app/api/axios.ts
+++ front/app/api/axios.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-// axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true;
 
 // Axios 인스턴스 생성
 export const axiosBase = axios.create({
   baseURL: "https://testserver-h4i7.onrender.com/api/",
+  // baseURL: "https://testserver-h4i7.onrender.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
