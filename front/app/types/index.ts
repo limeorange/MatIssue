ba9e1c91c6448797e62a5bb3a4ebbaeef1d49dda
limeorange <happyshsh72@gmail.com>
2023-06-08 04:52:51 +0900

    Fix : Recipe 타입 변경

--- front/app/types/index.ts
+++ front/app/types/index.ts
@@ -10,20 +10,41 @@ export type User =
   | undefined;
 
 export type Recipe = {
-  created_at: string;
-  recipe_category?: string;
-  recipe_description?: string;
-  recipe_id: string;
-  recipe_info?: { serving: number; time: number; level: number };
-  recipe_ingredients?: { name: "string"; amount: "string" }[];
-  recipe_like: number;
-  recipe_sequence?: { step: number; picture: string; description: string }[];
-  recipe_thumbnail: string;
-  recipe_tip?: string;
   recipe_title: string;
-  recipe_video?: string;
+  recipe_thumbnail: string;
+  recipe_video: string;
+  recipe_description: string;
+  recipe_category: string;
+  recipe_info: {
+    serving: number;
+    time: number;
+    level: number;
+  };
+  recipe_ingredients: {
+    name: string;
+    amount: string;
+  }[];
+  recipe_sequence: {
+    step: number;
+    picture: string;
+    description: string;
+  }[];
+  recipe_tip: string;
+  recipe_id: string;
   recipe_view: number;
+  recipe_like: number;
   user_id: string;
   user_nickname: string;
-  _id?: string;
+  created_at: string;
+
+  // 댓글 관련 Data Type 정의
+  comments: {
+    comment_author: string;
+    comment_text: string;
+    comment_like: number;
+    comment_id: string;
+    created_at: string;
+    comment_parent: string;
+    updated_at: string;
+  };
 };
