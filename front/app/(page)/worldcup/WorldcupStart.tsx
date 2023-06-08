4022642c1d1f749b4d14af1a5bcb28725f3952fb
[SW4/2반] 김동균 <potter1185@gmail.com>
2023-06-08 20:51:40 +0900

    Feat: 이상형월드컵 기능 구현

--- front/app/(page)/worldcup/WorldcupStart.tsx
+++ front/app/(page)/worldcup/WorldcupStart.tsx
@@ -4,70 +4,57 @@ import styled from "styled-components";
 import Image from "next/image";
 import { getAllRecipes } from "@/app/api/recipe";
 
-// Assuming that a recipe has an id, a title, and a thumbnail image.
-interface Recipe {
+type Recipe = {
   recipe_id: string;
   recipe_title: string;
   recipe_thumbnail: string;
-}
+};
 
 const GameStart: React.FC = () => {
-  const [recipes, setRecipes] = useState<Recipe[]>([]);
-  const [rounds, setRounds] = useState<Recipe[][]>([]);
-  const [roundIndex, setRoundIndex] = useState(0);
-  const [totalRounds, setTotalRounds] = useState(0); // 추가: 전체 라운드 수
+  const [foods, setFoods] = useState<Recipe[]>([]);
+  const [displays, setDisplays] = useState<Recipe[]>([]);
+  const [winners, setWinners] = useState<Recipe[]>([]);
 
-  // Load recipes initially
   useEffect(() => {
     const fetchRecipes = async () => {
-      const allRecipes = await getAllRecipes();
-      setRecipes(allRecipes);
+      try {
+        const recipes = await getAllRecipes();
+        recipes.sort(() => Math.random() - 0.5);
+        const selectedRecipes = recipes.slice(0, 32); // 랜덤으로 32개의 레시피 선택
+        setFoods(selectedRecipes);
+        setDisplays([selectedRecipes[0], selectedRecipes[1]]);
+      } catch (error) {
+        // Handle error
+      }
     };
+
     fetchRecipes();
   }, []);
 
-  // When recipes are set, select 16 random ones for the game
-  useEffect(() => {
-    if (recipes.length > 0) {
-      const gameRecipes = [...recipes]
-        .sort(() => Math.random() - 0.5)
-        .slice(0, 16);
-      const numberOfRounds = Math.ceil(Math.log2(gameRecipes.length)); // 추가: 전체 라운드 수 계산
-      setRounds([gameRecipes]);
-      setTotalRounds(numberOfRounds); // 추가: 전체 라운드 수 설정
+  const clickHandler = (food: Recipe) => () => {
+    if (foods.length <= 2) {
+      if (winners.length === 0) {
+        setDisplays([food]);
+      } else {
+        const updatedFood = [...winners, food];
+        setFoods(updatedFood);
+        setDisplays([updatedFood[0], updatedFood[1]]);
+        setWinners([]);
+      }
+    } else if (foods.length > 2) {
+      setWinners([...winners, food]);
+      setDisplays([foods[2], foods[3]]);
+      setFoods(foods.slice(2));
     }
-  }, [recipes]);
-
-  const handleSelectRecipe = (selectedRecipe: Recipe) => {
-    // Move selected recipe to next round
-    const currentRound = rounds[rounds.length - 1];
-    const nextRound = currentRound.filter(
-      (recipe) => recipe.recipe_id === selectedRecipe.recipe_id
-    );
-
-    setRounds([...rounds, nextRound]);
-
-    // Update round index
-    setRoundIndex(roundIndex + 1);
   };
-
-  const currentMatch = rounds[0]
-    ? rounds[0].slice(roundIndex, roundIndex + 2)
-    : [];
 
   return (
     <WorldcupLayout>
       <GameHeader>레시피 이상형 월드컵!</GameHeader>
-      <RoundHeader>
-        {/* 추가: 현재 라운드와 전체 라운드 수 출력 */}
-        {rounds.length > 0 && `라운드 (${roundIndex + 1}/${totalRounds})`}
-      </RoundHeader>
+
       <CardContainer>
-        {currentMatch.map((recipe: Recipe) => (
-          <Card
-            key={recipe.recipe_id}
-            onClick={() => handleSelectRecipe(recipe)}
-          >
+        {displays.map((recipe) => (
+          <Card key={recipe.recipe_id} onClick={clickHandler(recipe)}>
             <h2>{recipe.recipe_title}</h2>
             <Image
               src={recipe.recipe_thumbnail}
@@ -77,6 +64,10 @@ const GameStart: React.FC = () => {
             />
           </Card>
         ))}
+
+        {displays.length === 1 && (
+          <h2>짜잔! 우승 레시피입니다: {displays[0].recipe_title}</h2>
+        )}
       </CardContainer>
     </WorldcupLayout>
   );
@@ -94,11 +85,6 @@ const GameHeader = styled.h1`
   margin-bottom: 2rem;
 `;
 
-const RoundHeader = styled.h2`
-  text-align: center;
-  margin-bottom: 1rem;
-`;
-
 const CardContainer = styled.div`
   display: flex;
   justify-content: space-between;
@@ -109,10 +95,5 @@ const Card = styled.div`
   border: 1px solid #000;
   padding: 1rem;
   box-sizing: border-box;
-`;
-
-const ImageWrapper = styled.div`
-  position: relative;
-  width: 32rem;
-  height: 32rem;
+  cursor: pointer;
 `;
