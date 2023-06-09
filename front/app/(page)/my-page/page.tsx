bd941c03b091dd1ed1fe4f496059ad27e79f427e
jang_yoonsu <shagrat8546@gmail.com>
2023-06-09 13:01:00 +0900

    Refactor : 마이페이지 데이터 관리 로직 수정

--- front/app/(page)/my-page/page.tsx
+++ front/app/(page)/my-page/page.tsx
@@ -2,10 +2,10 @@ import { getRecipesByUserIdSS } from "@/app/action/getRecipesByUserId";
 import MyPage from "./my-page";
 
 const PostMyPage = async () => {
-  // 지금 로그인된 유저가 작성한 레시피를 패치해와서 변수에 담음
-  const currentUserRecipes = await getRecipesByUserIdSS();
+  // 지금 로그인된 유저가 작성한 레시피룰 패치해와서 변수에 담음
+  const initialCurrentUserRecipes = await getRecipesByUserIdSS();
 
-  return <MyPage currentUserRecipes={currentUserRecipes} />;
+  return <MyPage initialCurrentUserRecipes={initialCurrentUserRecipes} />;
 };
 
 export default PostMyPage;
