4761b7f68ec8bfb9f7fea0654f3f0ab9c436a11a
jang_yoonsu <shagrat8546@gmail.com>
2023-06-10 00:17:37 +0900

    Feat : 404에러 페이지 구현

---
+++ front/app/not-found.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Image from "next/image";
+import styled from "styled-components";
+
+const NotFound = () => {
+  return (
+    <ErrorPageContainer>
+      <ErrorTitle>404 ERROR!</ErrorTitle>
+      <ErrorImageWrapper>
+        <Image
+          src="/images/error/404.png"
+          height={200}
+          width={240}
+          alt="404_icon"
+        />
+      </ErrorImageWrapper>
+    </ErrorPageContainer>
+  );
+};
+
+export default NotFound;
+
+const ErrorPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100wh;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ErrorTitle = styled.div`
+  font-size: 40px;
+`;
+
+const ErrorImageWrapper = styled.div`
+  position: relative;
+`;
