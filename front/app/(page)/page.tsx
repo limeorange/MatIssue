002b7cedc8e3ba52fb2e69591a842210689fdc63
jang_yoonsu <shagrat8546@gmail.com>
2023-06-11 14:07:20 +0900

    Refactor : 메인페이지 데이터 부분적으로 클라이언트렌더링으로 변경

--- front/app/(page)/page.tsx
+++ front/app/(page)/page.tsx
@@ -9,29 +9,9 @@ import { Recipe } from "../types";
 import MainPageClient from "./MainClient";
 
 const Home = async () => {
-  const recipes = await getAllRecipes();
   const bestRecipes = await getRecipesByPopularity();
-  const newestRecipes = await getRecipesByLastest();
-  const singleRecipes = await getRecipesBySingle();
-  const vegetarianRecipes = await getRecipesByVegetarian();
 
-  function shuffleRecipes(array: Recipe[]) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  }
-
-  return (
-    <MainPageClient
-      recipes={recipes}
-      bestRecipes={bestRecipes}
-      newestRecipes={newestRecipes}
-      singleRecipes={shuffleRecipes(singleRecipes)}
-      vegetarianRecipes={shuffleRecipes(vegetarianRecipes)}
-    />
-  );
+  return <MainPageClient bestRecipes={bestRecipes} />;
 };
 
 export default Home;
