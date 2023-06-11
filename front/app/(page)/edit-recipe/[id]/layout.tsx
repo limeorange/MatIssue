c31a502977f983eafc85160ea7d4e5344225ca3a
jang_yoonsu <shagrat8546@gmail.com>
2023-06-11 20:36:40 +0900

    Fix : 청크로드 에러 테스트

--- front/app/(page)/edit-recipe/[id]/layout.tsx
+++ front/app/(page)/edit-recipe/[id]/layout.tsx
@@ -6,32 +6,32 @@ import { User } from "@/app/types";
 import ConfirmModal from "@/app/components/UI/ConfirmModal";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const { data: currentUser, isLoading } = useQuery<User>(["currentUser"]);
+  // const { data: currentUser, isLoading } = useQuery<User>(["currentUser"]);
 
-  const router = useRouter();
+  // const router = useRouter();
 
-  if (isLoading) {
-    // 로딩 중이라면 아무것도 렌더링하지 않음
-    return null;
-  }
+  // if (isLoading) {
+  //   // 로딩 중이라면 아무것도 렌더링하지 않음
+  //   return null;
+  // }
 
-  const handleLogin = () => {
-    router.replace("/auth/login");
-  };
+  // const handleLogin = () => {
+  //   router.replace("/auth/login");
+  // };
 
-  const handleCancel = () => {
-    router.replace("/");
-  };
+  // const handleCancel = () => {
+  //   router.replace("/");
+  // };
 
-  if (!currentUser) {
-    return (
-      <ConfirmModal
-        message="글을 수정하시려면 로그인이 필요합니다."
-        onConfirm={handleLogin}
-        onCancel={handleCancel}
-      />
-    );
-  }
+  // if (!currentUser) {
+  //   return (
+  //     <ConfirmModal
+  //       message="글을 수정하시려면 로그인이 필요합니다."
+  //       onConfirm={handleLogin}
+  //       onCancel={handleCancel}
+  //     />
+  //   );
+  // }
 
   return <>{children}</>;
 }
