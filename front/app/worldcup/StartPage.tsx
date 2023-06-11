a048412073df328d5b4ce2fe54950dad12ba21b9
[SW4/2반] 김동균 <potter1185@gmail.com>
2023-06-11 19:33:44 +0900

    Design: button쉐도우 조정

--- front/app/worldcup/StartPage.tsx
+++ front/app/worldcup/StartPage.tsx
@@ -150,7 +150,7 @@ const ButtonBox = styled.div<StyledComponentProps>`
   margin-top: 1rem;
 
   & Button {
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.1);
     transition: transform 0.3s ease;
     margin-right: 1rem;
 
