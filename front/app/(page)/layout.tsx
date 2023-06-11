6b99077bbe3d77a20c8d54fb6636c3dc0f68bbfd
jang_yoonsu <shagrat8546@gmail.com>
2023-06-12 00:26:58 +0900

    Refactor : 안쓰는 경로 삭제

--- front/app/(page)/layout.tsx
+++ front/app/(page)/layout.tsx
@@ -1,7 +1,6 @@
 import Header from "../components/header/Header";
 import { getCurrentUserSS } from "../action/getCurrentUser";
 import Footer from "../components/footer/Footer";
-import BottomNavigationBar from "../components/bottom-nav/BottomNavigationBar";
 
 export default async function Layout({
   children,
