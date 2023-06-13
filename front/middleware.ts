56cc08d9f36fbd84824f8555586478d9385b7fe0
jang_yoonsu <shagrat8546@gmail.com>
2023-06-13 13:42:29 +0900

    Fix : 미들웨어 오류 수정

--- front/middleware.ts
+++ front/middleware.ts
@@ -3,24 +3,16 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  if (request.url.includes("/my-page") && !request.cookies.get("session-id")) {
-    return NextResponse.redirect(new URL("/auth/login", request.url));
-  }
-  if (
-    request.url.includes("/add-recipe") &&
-    !request.cookies.get("session-id")
-  ) {
-    return NextResponse.redirect(new URL("/auth/login", request.url));
-  }
-  if (
-    request.url.includes("/edit-recipe") &&
-    !request.cookies.get("session-id")
-  ) {
-    return NextResponse.redirect(new URL("/auth/login", request.url));
-  }
-  if (request.url.includes("/admin") && !request.cookies.get("session-id")) {
+  if (!request.cookies.get("session-id")) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
 }
 
-export const config = {};
+export const config = {
+  matcher: [
+    "/my-page/:path*",
+    "/add-recipe/:path*",
+    "/edit-recipe/:path*",
+    "/admin/:path*",
+  ],
+};
