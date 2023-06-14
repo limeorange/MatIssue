7108a5ab371898385c8495c05c18487f52ce9ad1
[SW4/2반] 김동균 <potter1185@gmail.com>
2023-06-14 13:49:11 +0900

    Feat: 카카오톡 공유할때 레시피 사진도 공유됨

--- front/app/utils/worldcupKakaoShare.tsx
+++ front/app/utils/worldcupKakaoShare.tsx
@@ -1,15 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import { getRecipeById } from "@/app/api/recipe";
+import { useSearchParams } from "next/navigation";
+
+type Recipe = {
+  recipe_id: string;
+  recipe_title: string;
+  recipe_thumbnail: string;
+};
 
 const WorldcupKakaoShareButton = () => {
   const shareUrl = typeof window !== "undefined" ? window.location.href : "";
+  const searchParams = useSearchParams();
+  const id = searchParams?.get("winnerId");
+
+  const [recipe, setRecipe] = useState<Recipe | null>(null);
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
+    if (id) {
+      getRecipeById(id).then(setRecipe);
+    }
+  }, [id]);
+
+  useEffect(() => {
+    if (recipe && typeof window !== "undefined") {
       const { Kakao } = window;
 
       if (!Kakao.isInitialized()) {
-        Kakao.init("8f8a30270178844fc3a8e4f1df9a1c22");
+        Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY);
       }
 
       Kakao.Link.createDefaultButton({
@@ -18,7 +36,7 @@ const WorldcupKakaoShareButton = () => {
         content: {
           title: "맛이슈 이상형 월드컵",
           description: "뭐 먹을지 고민될땐?",
-          imageUrl: "/images/logo1.png",
+          imageUrl: recipe.recipe_thumbnail,
           link: {
             mobileWebUrl: shareUrl,
             webUrl: shareUrl,
@@ -35,7 +53,7 @@ const WorldcupKakaoShareButton = () => {
         ],
       });
     }
-  }, [shareUrl]);
+  }, [recipe, shareUrl]);
 
   return (
     <>
