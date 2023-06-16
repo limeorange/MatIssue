66e9e9084d0ac7663767f36571572b3fe8e38a4f
jang_yoonsu <shagrat8546@gmail.com>
2023-06-16 14:20:52 +0900

    Feat : 로그인시 auth 페이지 리다이렉션 추가

--- front/middleware.ts
+++ front/middleware.ts
@@ -3,6 +3,13 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+  if (
+    request.cookies.get("session-id") &&
+    request.nextUrl.pathname.startsWith("/auth")
+  ) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
   if (!request.cookies.get("session-id")) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
@@ -14,5 +21,6 @@ export const config = {
     "/add-recipe/:path*",
     "/edit-recipe/:path*",
     "/admin/:path*",
+    "/auth/:path*",
   ],
 };
