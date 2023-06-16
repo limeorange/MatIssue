90da870f0bff4781f515df39611459fabcfaf223
jang_yoonsu <shagrat8546@gmail.com>
2023-06-16 18:20:29 +0900

    Fix : 회원탈퇴 기능 오류 해결

--- front/app/components/my-page/AccountDeletion.tsx
+++ front/app/components/my-page/AccountDeletion.tsx
@@ -1,57 +1,51 @@
 "use client";
 
 import styled from "styled-components";
-import React, {  useState } from "react";
+import React, { useState } from "react";
 import PasswordModalComponent from "../UI/PasswordModal";
 import { useRouter } from "next/navigation";
+import PasswordModal from "../UI/PasswordModal";
 import { axiosBase } from "@/app/api/axios";
 import Cookies from "js-cookie";
-import { User } from "../../types/index";
+import { toast } from "react-hot-toast";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { loginState } from "@/app/store/authAtom";
 
-
-const AccountDeletion= ({id, password} : {id: any, password: any}) => {
+const AccountDeletion = ({ id }: { id: string }) => {
   const router = useRouter();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [enteredPassword, setEnteredPassword] = useState("");
-
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [enteredPassword, setEnteredPassword] = useState<string>("");
+  const setIsLoggedIn = useSetRecoilState<boolean>(loginState);
 
   const closeModal = () => {
     setIsModalOpen(false);
     setEnteredPassword("");
   };
-  
+
   const openModal = () => {
     setIsModalOpen(true);
   };
 
   // 회원 탈퇴 컴포넌트
   const handleDeleteAccount = async () => {
-   
-    
+    try {
+      const response = await axiosBase.delete("users", {
+        data: {
+          user_id: id,
+          password: enteredPassword,
+        },
+      });
 
-    console.log("id : ", id);
-    console.log("password : ", enteredPassword);
+      toast.success("계정이 성공적으로 삭제되었습니다.");
+      Cookies.remove("session_id");
+      setIsLoggedIn(false);
 
-    // try {
-    //   const response = await axiosBase.delete("users", {
-    //     data: {
-    //       user_id: id,
-    //       password: password,
-    //     },
-    //   });
-    //   console.log("delete 후 response : ", response);
-
-    //   if (response.status === 200) {
-    //     Cookies.remove("session_id");
-    //     console.log("계정이 삭제되었습니다.");
-    //     await axiosBase.post("users/logout");
-    //     router.push("/");
-    //   } else {
-    //     console.error("계정 삭제에 실패하였습니다.");
-    //   }
-    // } catch (error) {
-    //   console.error("Error occurred while deleting account:", error);
-    // }
+      router.push("/");
+    } catch (error: any) {
+      toast.error(
+        error.reponse ? error.reponse.data.detail : "회원탈퇴를 실패했습니다."
+      );
+    }
     closeModal();
   };
 
@@ -59,28 +53,27 @@ const AccountDeletion= ({id, password} : {id: any, password: any}) => {
     setEnteredPassword(event.target.value);
   };
 
- 
   return (
     <>
       <DeletionAndArrow>
-          <AccountDelete onClick={openModal}>회원 탈퇴</AccountDelete>
-          <ArrowImage src="/images/right-arrow.svg" alt="arrow-right" />
-          {isModalOpen && (
-            <StyledPasswordModal
-              icon={<AlertImage src="/images/alert.png" alt="alert" />}
-              message="정말 탈퇴 하시겠습니까?"
-              onCancel={closeModal}
-              onConfirm={handleDeleteAccount}
-            />
-          )}
+        <AccountDelete onClick={openModal}>회원 탈퇴</AccountDelete>
+        <ArrowImage src="/images/right-arrow.svg" alt="arrow-right" />
+        {isModalOpen && (
+          <PasswordModal
+            icon={<AlertImage src="/images/alert.png" alt="alert" />}
+            message="탈퇴하시려면 비밀번호를 입력해주세요."
+            onCancel={closeModal}
+            onConfirm={handleDeleteAccount}
+            onPasswordChange={handlePasswordChange}
+            enteredPassword={enteredPassword}
+          />
+        )}
       </DeletionAndArrow>
-     
     </>
   );
 };
 
 export default AccountDeletion;
-
 
 const AccountDelete = styled.div`
   font-size: 14px;
@@ -106,16 +99,12 @@ display: none;
 `;
 
 const DeletionAndArrow = styled.div`
-display:flex;
-align-items: center;
-margin-bottom: 2rem;
+  display: flex;
+  align-items: center;
+  margin-bottom: 2rem;
 `;
 
 const AlertImage = styled.img`
   width: 3rem;
   height: 3rem;
 `;
-
-const StyledPasswordModal = styled.div`
-`;
-
