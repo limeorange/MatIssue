3471b474738b4b9dca91a7912b17c76fcc253446
jang_yoonsu <shagrat8546@gmail.com>
2023-06-18 15:33:24 +0900

    Comment : 메인 클라이언트 주석 추가

--- front/app/(page)/MainClient.tsx
+++ front/app/(page)/MainClient.tsx
@@ -11,7 +11,9 @@ import styled from "styled-components";
 import { Recipe } from "../types";
 import MobileCategory from "../components/main-page/mobile/MobileCategory";
 
-// 메인화면에서 가장 위에 보이는 bestRecipes만 서버사이드 렌더링
+/** 유저경험을 향상시키기위해 메인화면에서 가장 위에 보이는 bestRecipes만 서버사이드 렌더링
+ * 상대적으로 아래있는 다른 컴포넌트들은 클라이언트사이드 렌더링으로 진행
+ * */
 const MainPageClient = ({ bestRecipes }: { bestRecipes: Recipe[] }) => {
   return (
     <>
