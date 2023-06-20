415df118698a8b5c229bcadbabc480c33888bae0
jang_yoonsu <shagrat8546@gmail.com>
2023-06-20 23:26:10 +0900

    Feat : 다크모드 토글시 body태그 색상 변경

--- front/app/components/header/DarkModeBtn.tsx
+++ front/app/components/header/DarkModeBtn.tsx
@@ -2,18 +2,28 @@
 
 import darkModeAtom from "@/app/store/darkModeAtom";
 import Image from "next/image";
+import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 
 const DarkmodeBtn = () => {
-  const [darkMode, setDarkMode] = useRecoilState(darkModeAtom);
+  const [isDarkMode, setIsDarkMode] = useRecoilState(darkModeAtom);
 
-  console.log(darkMode);
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.style.backgroundColor = "#2A2E41";
+    } else {
+      document.body.style.backgroundColor = "#fff";
+    }
+  }, [isDarkMode]);
 
   return (
-    <ToggleContainer onClick={() => setDarkMode(!darkMode)} darkMode={darkMode}>
+    <ToggleContainer
+      onClick={() => setIsDarkMode(!isDarkMode)}
+      isDarkMode={isDarkMode}
+    >
       <Icon src="/images/darkMode/moon.svg" alt="Moon" width={15} height={15} />
-      <ToggleBtn darkMode={darkMode} />
+      <ToggleBtn darkMode={isDarkMode} />
       <Icon src="/images/darkMode/sun.svg" alt="Sun" width={15} height={15} />
     </ToggleContainer>
   );
@@ -21,7 +31,7 @@ const DarkmodeBtn = () => {
 
 export default DarkmodeBtn;
 
-const ToggleContainer = styled.div<{ darkMode: boolean }>`
+const ToggleContainer = styled.div<{ isDarkMode: boolean }>`
   display: none;
   @media (min-width: 1024px) {
     display: flex;
@@ -30,21 +40,21 @@ const ToggleContainer = styled.div<{ darkMode: boolean }>`
     height: 3rem;
     border-radius: 10rem;
     background-color: ${(props) =>
-      props.darkMode ? props.theme.lightNavy : "#aaa"};
+      props.isDarkMode ? props.theme.lightNavy : "#aaa"};
     padding: 0.25rem;
     cursor: pointer;
     position: relative;
   }
 `;
 
-const ToggleBtn = styled.div<{ darkMode: boolean }>`
+const ToggleBtn = styled.div<{ isDarkMode: boolean }>`
   z-index: 1;
   width: 2.5rem;
   height: 2.5rem;
   background-color: white;
   border-radius: 5rem;
   transition: transform 0.2s ease-in-out;
-  transform: translateX(${(props) => (props.darkMode ? "2.5rem" : "0")});
+  transform: translateX(${(props) => (props.isDarkMode ? "2.5rem" : "0")});
 `;
 
 const Icon = styled(Image)`
