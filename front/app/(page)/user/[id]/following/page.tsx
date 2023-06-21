58b15a03c11fdfca56c4c1c507b8a504bb5f2d74
limeorange <happyshsh72@gmail.com>
2023-06-22 02:14:58 +0900

    Feat : 팔로잉 컴포넌트에 currentChefSubscriptions 데이터 넘겨줌

--- front/app/(page)/user/[id]/following/page.tsx
+++ front/app/(page)/user/[id]/following/page.tsx
@@ -1,14 +1,18 @@
-import { getChefByUserId } from "@/app/api/user";
+import { getChefByUserId, getUserSubscriptions } from "@/app/api/user";
 import Following from "./FollowingPage";
 
 const FollowingPage = async ({ params }: { params: { id: string } }) => {
   const userProfileId = params.id;
   const initialCurrentChef = await getChefByUserId(userProfileId);
 
+  /** 프로필 유저의 Subscriptions 배열 */
+  const currentChefSubscriptions = await getUserSubscriptions(userProfileId);
+
   return (
     <Following
       userProfileId={userProfileId}
       initialCurrentChef={initialCurrentChef}
+      currentChefSubscriptions={currentChefSubscriptions}
     />
   );
 };
