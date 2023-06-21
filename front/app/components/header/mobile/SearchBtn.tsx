bd2ad3e8e5f3756667e6a7344b32782d08374dd3
jang_yoonsu <shagrat8546@gmail.com>
2023-06-21 21:28:20 +0900

    Feat : 서치버튼 다크모드 구현

--- front/app/components/header/mobile/SearchBtn.tsx
+++ front/app/components/header/mobile/SearchBtn.tsx
@@ -2,14 +2,18 @@ import Image from "next/image";
 import { useState } from "react";
 import styled from "styled-components";
 import SearchModal from "./SearchModal";
+import { useRecoilValue } from "recoil";
+import darkModeAtom from "@/app/store/darkModeAtom";
 
 const SearchBtn = () => {
   const [isModal, setIsModal] = useState<boolean>(false);
+  const isDarkMode = useRecoilValue(darkModeAtom);
 
   return (
     <>
       <SearchModal isModal={isModal} setIsModal={setIsModal} />
       <SearchBtnContainer
+        isDarkMode={isDarkMode}
         onClick={() => {
           setIsModal(true);
           document.body.style.overflow = "hidden";
@@ -28,8 +32,13 @@ const SearchBtn = () => {
 
 export default SearchBtn;
 
-const SearchBtnContainer = styled.div`
+const SearchBtnContainer = styled.div<{ isDarkMode: boolean }>`
   diplay: block;
+
+  filter: ${(props) =>
+    props.isDarkMode
+      ? "invert(89%) sepia(27%) saturate(436%) hue-rotate(334deg) brightness(105%) contrast(104%)"
+      : "invert(18%) sepia(10%) saturate(2848%) hue-rotate(357deg) brightness(103%) contrast(82%)"};
 
   @media (min-width: 1024px) {
     display: none;
