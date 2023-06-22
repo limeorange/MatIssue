944969398216dba16e7ea77bf937eebd0cbb9805
jang_yoonsu <shagrat8546@gmail.com>
2023-06-22 14:02:55 +0900

    Feat : 다크모드 타입 파일 구현

---
+++ front/app/types/darkMode.d.ts
@@ -0,0 +1,19 @@
+export type Theme = {
+  deepYellow: string;
+  yellow: string;
+  lightYellow: string;
+  brown: string;
+  lightBrown: string;
+  deepNavy: string;
+  navy: string;
+  lightNavy: string;
+  deepGrey: string;
+  grey: string;
+  lightGrey: string;
+  white: string;
+};
+
+export type DarkModeProps = {
+  isDarkMode: boolean;
+  theme: Theme;
+};
