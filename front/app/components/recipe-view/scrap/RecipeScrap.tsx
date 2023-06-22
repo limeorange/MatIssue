7229378c84ea340ef0df2c2fa2c151cc4d394339
limeorange <happyshsh72@gmail.com>
2023-06-22 17:11:00 +0900

    Feat : 게시글 스크랩 다크모드 구현

--- front/app/components/recipe-view/scrap/RecipeScrap.tsx
+++ front/app/components/recipe-view/scrap/RecipeScrap.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import darkModeAtom from "@/app/store/darkModeAtom";
 import Image from "next/image";
 import { useEffect } from "react";
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
 
 type UserScrapProps = {
@@ -39,6 +41,9 @@ const RecipeScrap = ({
   setIsSaved,
   recipe_id,
 }: UserScrapProps) => {
+  // 다크 모드 상태 관리
+  const [isDarkMode, setIsDarkMode] = useRecoilState(darkModeAtom);
+
   // 처음 렌더링 시 클라이언트 사이드에서 로컬스토리지 받아오기 위한 의존성 관리
   // 스크랩 된 메모가 있으면 색칠되어있음
   useEffect(() => {
@@ -55,7 +60,7 @@ const RecipeScrap = ({
 
   return (
     <>
-      <ScrapButtonWrapper onClick={scrapClickHandler}>
+      <ScrapButtonWrapper isDarkMode={isDarkMode} onClick={scrapClickHandler}>
         <ScrapIcon>
           <Image
             src={
@@ -69,22 +74,24 @@ const RecipeScrap = ({
             style={{ objectFit: "cover", cursor: "pointer" }}
           />
         </ScrapIcon>
-        <ScrapTitle>스크랩</ScrapTitle>
+        <ScrapTitle isDarkMode={isDarkMode}>스크랩</ScrapTitle>
       </ScrapButtonWrapper>
     </>
   );
 };
 
 /** 스크랩 아이콘과 글자 묶는 Button */
-const ScrapButtonWrapper = styled.button`
+const ScrapButtonWrapper = styled.button<{ isDarkMode: boolean }>`
   display: flex;
   width: 10rem;
   height: 4.2rem;
   border-radius: 1.5rem;
   justify-content: center;
   align-items: center;
-  background-color: #ffffff;
+
   box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.3);
+  background-color: ${(props) =>
+    props.isDarkMode ? props.theme.lightNavy : "#ffffff"};
 
   @media (min-width: 1024px) {
     width: 12rem;
@@ -108,8 +115,9 @@ const ScrapIcon = styled.div`
 `;
 
 /** 스크랩 글자 Span */
-const ScrapTitle = styled.span`
+const ScrapTitle = styled.span<{ isDarkMode: boolean }>`
   font-size: 16px;
+  color: ${(props) => (props.isDarkMode ? props.theme.lightGrey : "#4F3D21")};
 
   @media (min-width: 1024px) {
     font-size: 18px;
