713afd9ec1260b5208d2699499ac96c167a4ea8c
limeorange <happyshsh72@gmail.com>
2023-06-22 20:50:28 +0900

    Fix : 팔로워 페이지 버튼 실시간 렌더링 이슈 해결

--- front/app/(page)/user/[id]/followers/page.tsx
+++ front/app/(page)/user/[id]/followers/page.tsx
@@ -1,18 +1,24 @@
-import { getChefByUserId, getUserFans } from "@/app/api/user";
+import getCurrentUser, { getChefByUserId, getUserFans } from "@/app/api/user";
 import Followers from "./FollowersPage";
 
 const FollowersPage = async ({ params }: { params: { id: string } }) => {
   const userProfileId = params.id;
+
+  /** 프로필 유저정보 (user_id, username, img, fans, subscriptions) */
   const initialCurrentChef = await getChefByUserId(userProfileId);
 
   /** 프로필 유저의 Fans 배열 */
   const currentChefFans = await getUserFans(userProfileId);
+
+  /** 로그인된 유저정보 */
+  const initialCurrentUser = await getCurrentUser();
 
   return (
     <Followers
       userProfileId={userProfileId}
       initialCurrentChef={initialCurrentChef}
       currentChefFans={currentChefFans}
+      initialCurrentUser={initialCurrentUser}
     />
   );
 };
