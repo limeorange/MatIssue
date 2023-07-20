8dbdbda12b8051d2e71a82672c53a4141c667c37
H0onnn <dukei2017@naver.com>
2023-07-20 16:32:43 +0900

    Style : API 요청 URL 변경

--- front/app/api/axios.ts
+++ front/app/api/axios.ts
@@ -10,7 +10,8 @@ export const axiosBase = axios.create({
   // baseURL: "https://matissue-1jim.onrender.com/api/",
   // baseURL: "https://matissue.p-e.kr/api",
   // baseURL: "https://mat-issue.onrender.com/api/",
-  baseURL: "https://kdt-sw-4-team10.elicecoding.com/api/",
+  // baseURL: "https://kdt-sw-4-team10.elicecoding.com/api/",
+  baseURL: "https://matissue.n-e.kr/api",
   headers: {
     "Content-Type": "application/json",
   },
